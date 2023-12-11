c9c62c4489ada16b2cecb70b023d0a5a2e8f6220
unknown <mursalinkhan552@gmail.com>
2023-12-11 16:49:03 +0600

    Try to  Responsive the  Project

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='mx-12'>
-            <div className="navbar bg-base-100">
-                <div className="flex-1">
-                    <a className="btn btn-ghost text-2xl font-bold hover:bg-warning">Knowledge Cafe</a>
+        <div className='mx-0 md:mx-12'>
+            <div className="navbar bg-base-100 px-0">
+                <div className=" flex md:flex-1">
+                    <a className="btn btn-ghost text-lg md:text-2xl font-bold hover:bg-warning mr-2">Knowledge Cafe</a>
 
                 </div>
-                <div className='mr-12'>
-                    <a className="btn btn-ghost text-xl hover:bg-warning">Home</a>
-                    <a className="btn btn-ghost text-xl hover:bg-warning">Features</a>
-                    <a className="btn btn-ghost text-xl hover:bg-warning">About</a>
+                <div className='mr-2 ms-4 md:mr-12'>
+                    <a className="btn btn-ghost px-1 md:px-4 text-lg md:text-xl hover:bg-warning">Home</a>
+                    <a className="btn btn-ghost px-1 md:px-4 text-lg md:text-xl hover:bg-warning">Features</a>
+                    <a className="btn btn-ghost px-1 md:px-4 text-lg md:text-xl hover:bg-warning">About</a>
                 </div>
                 <div className="flex-none gap-2">
-                    <div className="form-control">
-                        <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                    <div className="form-control ">
+                        <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto hidden md:block" />
                     </div>
                     <div className="dropdown dropdown-end">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
